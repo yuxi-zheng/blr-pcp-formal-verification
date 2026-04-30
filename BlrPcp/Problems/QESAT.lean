409b26f9677f5c34291a41f965a845fd3491e688
vincentpalma <10133632+vincentpalma@users.noreply.github.com>
2026-04-30 20:37:28 +0200

    stated lpcp to pcp lemma for F_2

--- BlrPcp/Problems/QESAT.lean
+++ BlrPcp/Problems/QESAT.lean
@@ -15,6 +15,7 @@ This file defines the QESAT language and the exponential-length PCP for it.
 -/
 
 open CPoly CMvPolynomial
+open scoped ENNReal
 
 abbrev QESAT (F : Type) [Field F] (n : ℕ) : Set (List (CMvPolynomial n F)) :=
   fun polys => (∀ p ∈ polys, p.totalDegree ≤ 2) ∧
@@ -37,4 +38,13 @@ theorem QESAT_exp_PCP {vars : ℕ} : ∃ (q : ℕ) (r : Polynomial ℕ),
     QESAT (ZMod 2) vars ∈
       PCP (QESAT.size (ZMod 2) vars) 0 (1 / 2) (ZMod 2)
         (fun n => 2 ^ n) (fun _ => q) r.eval := by
+  sorry
+
+theorem LPCP_to_PCP_ZMod2 {α : Type} (size : α → ℕ)
+    (ε_c ε_s : ℝ≥0∞) (ℓ q r : ℕ → ℕ) :
+    LPCP size ε_c ε_s (ZMod 2) ℓ q r
+    ⊆ PCP size ε_c (max (7/8) (ε_s + 1/100)) (ZMod 2)
+      (fun n => 2 ^ ℓ n)
+      (fun n => 3 + 2 * Nat.clog 2 (100 * q n) * q n)
+      (fun n => r n + (2 + Nat.clog 2 (100 * q n)) * ℓ n) :=
   sorry
