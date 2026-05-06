3ad56891bfed38c025670ba7c614bb6bfa4b5a96
vidick <thomas.vidick@epfl.ch>
2026-05-06 06:55:30 +0200

    edit

--- BlrPcp/GH/Background.lean
+++ BlrPcp/GH/Background.lean
@@ -1,9 +1,3 @@
-/-
-Copyright (c) 2026 Thomas Vidick. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Thomas Vidick
--/
-
 import Mathlib.Analysis.Matrix.Order
 import Mathlib.LinearAlgebra.Matrix.Permutation
 
--- BlrPcp/GH/GowersHatami.lean
+++ BlrPcp/GH/GowersHatami.lean
@@ -1,8 +1,3 @@
-/-
-Copyright (c) 2026 Thomas Vidick. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Thomas Vidick
--/
 import BlrPcp.GH.Background
 
 /-!
